4902d03e2a91be76a67c4f823f34263f2e84b801
IsmaelU <ismealqeel@hotmail.co.uk>
2021-01-25 00:16:35 +0000

    Solved Problem 3- May return to in the future to find a better solution

--- Solutions/Problem 3.py
+++ Solutions/Problem 3.py
@@ -1,11 +1,21 @@
-#!/usr/bin/python3.8
-# NOT FINISHED
+#!/usr/bin/python3
 # Problem 3- Largest prime factor
 # https://projecteuler.net/problem=3
-# Answer = 
+# Answer = 6857
+
+def prime_factors(factors):
+    prime_list = []
+    for factor in factors:
+        prime = [i for i in range (2,factor) if factor % i == 0]
+        if not prime: 
+            prime_list.append(factor)
+    return max(prime_list)
+
 
 def largestprimefactor(num):
-    factors = (i for i in range(1,num) if num % i == 0)
-    return(factors)                  
+    factors = [i for i in range(1,num) if num % i == 0]
+    return(prime_factors(factors))
 
-print(largestprimefactor(5555))
+
+
+print(largestprimefactor(600851475143))
