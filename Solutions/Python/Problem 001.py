965d0b2f361696e4a1ce81db4e939f1ddad53923
IsmaelU <ismealqeel@hotmail.co.uk>
2021-04-19 14:53:19 +0100

    Solved Problem 12. Takes too long though

--- Solutions/Python/Problem 001.py
+++ Solutions/Python/Problem 001.py
@@ -6,11 +6,18 @@ def question():
     print("Find the sum of all the multiples of 3 or 5 below 1000.")
 
 
-def compute(bound):
+def solve(bound):
     return sum(i for i in range(1,bound) if i % 3 == 0 or i % 5 == 0)
+
+#def SumDivisbleBy(n , p):
+#    return int(n*(p/n)*((p/n)+1)/2)
+
+#def solve1(bound):
+#    return SumDivisbleBy(3,bound-1) + SumDivisbleBy(5,bound-1) - SumDivisbleBy(15,bound-1)
 
 def main():
     question()
-    print(compute(1000))
+    print(solve(1000))
+    #print(solve1(1000))
 
 main()
