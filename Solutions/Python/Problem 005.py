fcea44067b861fe5cdd96e74be1a17bcf2f979aa
IsmaelU <ismealqeel@hotmail.co.uk>
2021-04-23 15:24:13 +0100

    Refactored problem 5 and started on another problem

--- Solutions/Python/Problem 005.py
+++ Solutions/Python/Problem 005.py
@@ -2,22 +2,34 @@
 # https://projecteuler.net/problem=5
 # Answer = 21162960 
 
+def question():
+    print("""
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+    """)
+
+
 # Solution 1
 def multiple(number):
     for i in range(19,11,-1):
         if number  % i != 0:
-                return(False)
-    return(True)
+                return False
+    return True
 
-number = 20
-while True:
-    if multiple(number):
-        break
-    else:
-        number += 20
-
-print(number)
+def solve(num):
+    answer = num
+    while True:
+        if multiple(answer):
+            break
+        else:
+            answer += num
+    return answer
 
 # Solution 2
 
+def main():
+    question()
+    print(f"The answer is {solve(20)}")
 
+main()
---
+++ Solutions/Python/Problem 013.py
@@ -0,0 +1,42 @@
+# Project Euler Problem 14 - Longest Collatz sequence
+# https://projecteuler.net/problem-14
+# Answer =
+
+def question():
+    print("""
+The following iterative sequence is defined for the set of positive integers:
+
+n → n/2 (n is even)
+n → 3n + 1 (n is odd)
+
+Using the rule above and starting with 13, we generate the following sequence:
+13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+
+It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+
+Which starting number, under one million, produces the longest chain?
+""")
+
+def collatz(num):
+    sequence = [num,]
+    while sequence[-1] != 1:
+        if sequence[-1] % 2 == 0:
+            sequence.append(sequence[-1] / 2)
+        else:
+            sequence.append((3 * sequence[-1]) +1)
+    return len(sequence)
+
+
+def solve(bound):
+    longest_len = 0
+    for i in range (0,bound):
+        if collatz(i) > longest_len:
+            longest = i
+            longest_len = collatz(i)
+    return(longest,longest_len)
+
+def main():
+    question()
+    print(f"The answer is {solve(100)}")
+
+main()
