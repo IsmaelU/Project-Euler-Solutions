02ff9f6645975cefa6010286ffea116a20e2679c
Ismael U <ismealqeel@hotmail.co.uk>
2026-06-11 12:30:11 +0100

    Implement and fixed my old solution for Problem 9 (Rust)

--- Solutions/Rust/problem_009.rs
+++ Solutions/Rust/problem_009.rs
@@ -18,22 +18,23 @@ Find the product abc.
 
 fn generate(m: i32, n: i32) -> (i32, i32, i32) {
     let a: i32 = 2 * m * n;
-    let b: i32 = (m ^ 2) - (n ^ 2);
-    let c: i32 = (m ^ 2) + (n ^ 2);
+    let b: i32 = (m.pow(2)) - (n.pow(2));
+    let c: i32 = (m.pow(2)) + (n.pow(2));
     return (a, b, c);
 }
 
 fn solve(num: i32) -> i32 {
     let mut m = 2;
     let mut n = 1;
-    let (a, b, c) = generate(m, n);
+    let (mut a, mut b, mut c) = generate(m, n);
     while a + b + c != num {
         if m == n {
             m += 1;
             n = 1;
+        }else{
+            n += 1;   
         }
-        n += 1;
-        let (a, b, c) = generate(m, n);
+        (a, b, c) = generate(m, n);
     }
     a * b * c
 }
